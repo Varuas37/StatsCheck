305b6b04f1ac5f5cd554e21877b13df7abc1f185
Saurav Panthee <saurav.panthee@gmail.com>
2021-01-14 00:47:21 -0600

    Latest commit

--- screens/Dashboard/Dashboard.js
+++ screens/Dashboard/Dashboard.js
@@ -44,7 +44,7 @@ export default function Dashboard({ navigation }) {
           paddingTop: "10%",
         }}
       >
-        Basic Stats
+       A week in review
       </Text>
       <View
         style={{
@@ -132,7 +132,7 @@ export default function Dashboard({ navigation }) {
                 fontSize: 10,
               }}
             >
-              Videos Uploaded Last week
+              Videos Uploaded
             </Text>
           </View>
 
@@ -192,7 +192,7 @@ export default function Dashboard({ navigation }) {
                 fontWeight: "bold",
               }}
             >
-             +30
+             13% 
             </Text>
             <Text
               style={{
@@ -200,12 +200,32 @@ export default function Dashboard({ navigation }) {
                 fontSize: 10,
               }}
             >
-              followers gained
+              Engagement Rate
             </Text>
           </View>
       
         </View>
-      </View>
+   </View>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 24,
+          fontWeight: "bold",
+          paddingLeft: 15,
+          paddingTop: "10%",
+        }}
+      > 
+       Most Popular Tags
+      </Text>
+      <View>
+        <Text  style={{
+          color: "white",
+          
+        }}> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. </Text>
+        </View>
+
     </ScrollView>
   );
 }
