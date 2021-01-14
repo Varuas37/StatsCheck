9d753bdd65ef78f18446eb005b305810adf0f054
Saurav Panthee <saurav.panthee@gmail.com>
2021-01-14 01:39:00 -0600

    fixed layout issue

--- screens/Dashboard/Dashboard.js
+++ screens/Dashboard/Dashboard.js
@@ -1,231 +1,203 @@
-import React, { useState, Fragment } from "react";
-import { View, Text, ScrollView, Button, Image } from "react-native";
-import DashboardCard from "../../components/Card/DashboardCard";
-import ProgressBar from "react-native-progress/Bar";
-import StyleDashboard from "./StyleDashboard";
+import React, { useState, Fragment } from 'react';
+import { View, Text, ScrollView, Button, Image } from 'react-native';
+import DashboardCard from '../../components/Card/DashboardCard';
+import ProgressBar from 'react-native-progress/Bar';
+import StyleDashboard from './StyleDashboard';
 
 export default function Dashboard({ navigation }) {
-  const [filter, setFilter] = useState("Last 10 Days");
+	const [filter, setFilter] = useState('Last 10 Days');
 
-  const addAccount = () => {
-    navigation.navigate("AddUsername");
-  };
-  return (
-    <ScrollView style={{ backgroundColor: "black" }}>
-      <Text
-        style={{
-          color: "white",
-          fontSize: 24,
-          fontWeight: "bold",
-          paddingLeft: 15,
-          paddingTop: "10%",
-          marginBottom: 20,
-          // backgroundColor:"red"
-        }}
-      >
-        Overview
-      </Text>
-      <View
-        style={{
-          height: "100%",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <DashboardCard></DashboardCard>
-      </View>
-      <Text
-        style={{
-          color: "white",
-          fontSize: 24,
-          fontWeight: "bold",
-          paddingLeft: 15,
-          paddingTop: "10%",
-        }}
-      >
-       A week in review
-      </Text>
-      <View
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-          
-        }}
-      >
-        <View
-          style={{
-            display: "flex",
-            width: "90%",
-            backgroundColor: "#252525",
-            padding:25,
-            borderRadius: 10,
-            flexDirection: "row",
-            justifyContent:"space-between",
-            // alignItems:"center",
-            alignContent:"center",
-            flexWrap: 'wrap',
-          }}
-        >
-          <View
-            style={{
-              backgroundColor: "red",
-              width: "47%",
+	const addAccount = () => {
+		navigation.navigate('AddUsername');
+	};
+	return (
+		<ScrollView style={{ backgroundColor: 'black' }}>
+			<View>
+				<Text
+					style={{
+						color: 'white',
+						fontSize: 24,
+						fontWeight: 'bold',
+						paddingLeft: 15,
+						paddingTop: '10%',
+						marginBottom: 20,
+						// backgroundColor:"red"
+					}}
+				>
+					Overview
+				</Text>
+				<View
+					style={{
+						height: '100%',
+						flex: 1,
+						justifyContent: 'space-between',
+						alignItems: 'center',
+				
+					}}
+				>
+					<DashboardCard></DashboardCard>
+				</View>
+				<Text
+					style={{
+						color: 'white',
+						fontSize: 24,
+						fontWeight: 'bold',
+						paddingLeft: 15,
+						paddingTop: '5%',
+					}}
+				>
+					A week in review
+				</Text>
+				<View
+					style={{
+            display: 'flex',
+            // flex:1,
+            height:"50%",
+						flexDirection: 'column',
+						width: '100%',
+						justifyContent: 'center',
+						alignItems: 'center',
+					}}
+				>
+					<View
+						style={{
+							flex:1,
+							width: '90%',
+							backgroundColor: '#252525',
+							padding: 25,
               borderRadius: 10,
-              height: "50%",
-              alignItems: "center",
-              justifyContent: "center",
-             
-            }}
-          >
-            <Image
-              source={require("./clock.png")}
-              style={StyleDashboard.image}
-            />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 16,
-                fontWeight: "bold",
-              }}
-            >
-              8:00 AM
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 10,
-              }}
-            >
-              Best time for you to post
-            </Text>
-          </View>
-          <View
-            style={{
-           backgroundColor: "red",
-              width: "47%",
-              borderRadius: 10,
-              height: "50%",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Image
-              source={require("./video.png")}
-              style={StyleDashboard.image}
-            />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 16,
-                fontWeight: "bold",
-              }}
-            >
-             42
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 10,
-              }}
-            >
-              Videos Uploaded
-            </Text>
-          </View>
+              height:300,
+              marginTop:60,
+							flexDirection: 'row',
+							justifyContent: 'space-between',
+							// alignItems:"center",
+							alignContent: 'center',
+							flexWrap: 'wrap',
+						}}
+					>
+						<View
+							style={{
+								backgroundColor: 'red',
+								width: '47%',
+								borderRadius: 10,
+								height: '50%',
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<Image source={require('./clock.png')} style={StyleDashboard.image} />
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 16,
+									fontWeight: 'bold',
+								}}
+							>
+								8:00 AM
+							</Text>
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 10,
+								}}
+							>
+								Best time for you to post
+							</Text>
+						</View>
+						<View
+							style={{
+								backgroundColor: 'red',
+								width: '47%',
+								borderRadius: 10,
+								height: '50%',
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<Image source={require('./video.png')} style={StyleDashboard.image} />
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 16,
+									fontWeight: 'bold',
+								}}
+							>
+								42
+							</Text>
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 10,
+								}}
+							>
+								Videos Uploaded
+							</Text>
+						</View>
 
-          <View
-            style={{
-           backgroundColor: "red",
-              width: "47%",
-              borderRadius: 10,
-              height: "50%",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop:20,
-            }}
-          >
-            <Image
-              source={require("./followers.png")}
-              style={StyleDashboard.image}
-            />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 16, 
-                fontWeight: "bold",
-              }}
-            >
-             +30
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 10,
-              }}
-            >
-              followers gained
-            </Text>
-          </View>
+						<View
+							style={{
+								backgroundColor: 'red',
+								width: '47%',
+								borderRadius: 10,
+								height: '50%',
+								alignItems: 'center',
+								justifyContent: 'center',
+								marginTop: 20,
+							}}
+						>
+							<Image source={require('./followers.png')} style={StyleDashboard.image} />
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 16,
+									fontWeight: 'bold',
+								}}
+							>
+								+30
+							</Text>
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 10,
+								}}
+							>
+								followers gained
+							</Text>
+						</View>
 
-          <View
-            style={{
-           backgroundColor: "red",
-              width: "47%",
-              borderRadius: 10,
-              height: "50%",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop:20,
-            }}
-          >
-            <Image
-              source={require("./followers.png")}
-              style={StyleDashboard.image}
-            />
-            <Text
-              style={{
-                color: "white",
-                fontSize: 16, 
-                fontWeight: "bold",
-              }}
-            >
-             13% 
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                fontSize: 10,
-              }}
-            >
-              Engagement Rate
-            </Text>
-          </View>
-      
-        </View>
-   </View>
-      <Text
-        style={{
-          color: "white",
-          fontSize: 24,
-          fontWeight: "bold",
-          paddingLeft: 15,
-          paddingTop: "10%",
-        }}
-      > 
-       Most Popular Tags
-      </Text>
-      <View>
-        <Text  style={{
-          color: "white",
-          
-        }}> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. </Text>
-        </View>
-
-    </ScrollView>
-  );
+						<View
+							style={{
+								backgroundColor: 'red',
+								width: '47%',
+								borderRadius: 10,
+								height: '50%',
+								alignItems: 'center',
+								justifyContent: 'center',
+								marginTop: 20,
+							}}
+						>
+							<Image source={require('./followers.png')} style={StyleDashboard.image} />
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 16,
+									fontWeight: 'bold',
+								}}
+							>
+								13%
+							</Text>
+							<Text
+								style={{
+									color: 'white',
+									fontSize: 10,
+								}}
+							>
+								Engagement Rate
+							</Text>
+						</View>
+					</View>
+				</View>
+			</View>
+		</ScrollView>
+	);
 }
--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,55 +1,55 @@
-import React from "react";
-import { View, Text, ScrollView, Button } from "react-native";
-import DashboardCard from "../../components/Card/DashboardCard";
-import Center from "../../components/Center/Center";
-import VideoCard from "../../components/Card/VideoCard/VideoCard";
-import { VideoData } from "./Video/Data";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React from 'react';
+import { View, Text, ScrollView, Button } from 'react-native';
+import DashboardCard from '../../components/Card/DashboardCard';
+import Center from '../../components/Center/Center';
+import VideoCard from '../../components/Card/VideoCard/VideoCard';
+import { VideoData } from './Video/Data';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 export default function Profile({ navigation }) {
-  const handleClick = () => {
-    navigation.navigate("Videos");
-  };
-  return (
-    <ScrollView style={{ backgroundColor: "black" }}>
-      <Heading title="Overview"></Heading>
-      <View
-        style={{
-          height: "100%",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <DashboardCard data={VideoData.summary}></DashboardCard>
-      </View>
+	const handleClick = () => {
+		navigation.navigate('Videos');
+	};
+	return (
+		<ScrollView style={{ backgroundColor: 'black' }}>
+			<Heading title="Overview"></Heading>
+			<View
+				style={{
+					height: '100%',
+					flex: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<DashboardCard data={VideoData.summary}></DashboardCard>
+			</View>
 
-      <Heading title="Recent Videos"></Heading>
+			<Heading title="Recent Videos"></Heading>
 
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        {VideoData.results.map((item) => (
-            <VideoCard  key={item.id} data={item} navigation={navigation}></VideoCard>
-        ))}
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				{VideoData.results.map((item) => (
+					<VideoCard key={item.id} data={item} navigation={navigation}></VideoCard>
+				))}
 
-        <Button title="View All" color="white" onPress={handleClick}></Button>
-      </View>
-    </ScrollView>
-  );
+				<Button title="View All" color="red" onPress={handleClick} style={{padding:10}}></Button>
+			</View>
+		</ScrollView>
+	);
 }
 
 const Heading = ({ title }) => {
-  return (
-    <Text
-      style={{
-        color: "white",
-        fontSize: 24,
-        fontWeight: "bold",
-        paddingLeft: 15,
-        paddingTop: "10%",
-        marginBottom: 20,
-        // backgroundColor:"red"
-      }}
-    >
-      {title}
-    </Text>
-  );
+	return (
+		<Text
+			style={{
+				color: 'white',
+				fontSize: 24,
+				fontWeight: 'bold',
+				paddingLeft: 15,
+				paddingTop: '10%',
+				marginBottom: 20,
+				// backgroundColor:"red"
+			}}
+		>
+			{title}
+		</Text>
+	);
 };
